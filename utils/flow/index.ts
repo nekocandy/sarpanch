a82844db483589d6740160b73be13f0c6ccaaed6
Jeevika <heevk23499@gmail.com>
2024-01-19 22:47:35 +0530

    feat: initialize

---
+++ utils/flow/index.ts
@@ -0,0 +1,35 @@
+import { config } from '@onflow/fcl'
+import { env } from 'std-env'
+import flowJSON from '../../flow.json'
+
+export const network: 'mainnet' | 'testnet' | 'emulator' = (env.PUBLIC_FLOW_NETWORK as
+  | 'mainnet'
+  | 'testnet'
+  | 'emulator') || 'emulator'
+
+const fclConfigInfo = {
+  emulator: {
+    accessNode: 'http://127.0.0.1:8888',
+    discoveryWallet: 'http://localhost:8701/fcl/authn',
+    discoveryAuthInclude: [],
+  },
+  testnet: {
+    accessNode: 'https://rest-testnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+    discoveryAuthInclude: [],
+  },
+  mainnet: {
+    accessNode: 'https://rest-mainnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+    discoveryAuthInclude: [],
+  },
+}
+
+config({
+  'app.detail.title': 'Sarpanch',
+  'app.detail.icon': '',
+  'flow.network': network,
+  'accessNode.api': fclConfigInfo[network].accessNode,
+  'discovery.wallet': fclConfigInfo[network].discoveryWallet,
+  'discovery.authn.include': fclConfigInfo[network].discoveryAuthInclude,
+}).load({ flowJSON })
