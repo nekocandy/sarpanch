e786a7627fd31d724b818eaa59dbffc0bd062a76
Piyush <bhangalepiyush@gmail.com>
2024-01-20 21:36:57 +0530

    feat: generate blob

--- utils/image.ts
+++ utils/image.ts
@@ -1,7 +1,7 @@
 import { Canvas } from 'canvas-constructor/browser'
 import JoiningImage from '~/assets/joining.png'
 
-export default function generateJoiningImage(_joiningPosition?: number) {
+export default async function generateJoiningImage(_joiningPosition?: number) {
   const canvasElement = document.createElement('canvas')
   canvasElement.width = 820
   canvasElement.height = 312
@@ -9,10 +9,10 @@ export default function generateJoiningImage(_joiningPosition?: number) {
   const imageElement = document.createElement('img')
   imageElement.src = JoiningImage
 
-  return new Canvas(canvasElement)
+  return await new Canvas(canvasElement)
     .printImage(imageElement, 0, 0)
     .setColor('#ffffff')
     .setTextFont('bold 48px Inter')
     .printText('Member', 25, 280)
-    .toDataURL()
+    .toBlobAsync()
 }
