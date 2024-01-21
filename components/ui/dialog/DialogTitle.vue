92c5e451efb5fd17e8bed2cca61c75a384b83147
Piyush <bhangalepiyush@gmail.com>
2024-01-21 10:08:09 +0530

    feat: add dialog

---
+++ components/ui/dialog/DialogTitle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { DialogTitle, type DialogTitleProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DialogTitleProps & { class?: string }>()
+</script>
+
+<template>
+  <DialogTitle
+    v-bind="props"
+    :class="
+      cn(
+        'text-lg text-zinc-950 font-semibold leading-none tracking-tight dark:text-zinc-50',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </DialogTitle>
+</template>
